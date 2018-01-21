544ee5bf288d47cca80770c66b52439c3107f0d9
K <kwailamchan@hotmail.com>
2018-01-21 23:20:29 +0800

    [master] created models in cashflow. 	modified:   cashflow/admin.py 	new file:   cashflow/migrations/0004_expensedict.py 	new file:   cashflow/migrations/0005_incomedict.py 	modified:   cashflow/models.py

--- dashboard/cashflow/admin.py
+++ dashboard/cashflow/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
+from .models import ExpenseDict
+from .models import IncomeDict
 from .models import Expense
 from .models import Income
 
+admin.site.register(ExpenseDict)
+admin.site.register(IncomeDict)
 admin.site.register(Expense)
 admin.site.register(Income)
--- dashboard/cashflow/models.py
+++ dashboard/cashflow/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class ExpenseDict(models.Model):
+    itemid = models.BigIntegerField(default=0)
+    item = models.CharField(max_length=200)
+
+class IncomeDict(models.Model):
+    itemid = models.BigIntegerField(default=0)
+    item = models.CharField(max_length=200)
+
+
 class Expense(models.Model):
     year = models.IntegerField(default=0)
     month = models.IntegerField(default=0)
