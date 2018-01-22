55af642e6a0adcb4fa9643540c0d2632326a897b
K <kwailamchan@hotmail.com>
2018-01-22 23:44:11 +0800

    [master] created expense and income yearly tables. 	modified:   templates/index.html 	new file:   templates/report_yearly.html 	modified:   urls.py 	modified:   views.py

--- dashboard/cashflow/urls.py
+++ dashboard/cashflow/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('expensedict/', views.expensedict, name='expensedict'),
     path('expense/', views.expense, name='expense'),
     path('expense_monthly/', views.expense_monthly, name='expense_monthly'),
+    path('expense_yearly/', views.expense_yearly, name='expense_yearly'),
     path('incomedict/', views.incomedict, name='incomedict'),
     path('income/', views.income, name='income'),
     path('income_monthly/', views.income_monthly, name='income_monthly'),
+    path('income_yearly/', views.income_yearly, name='income_yearly'),
 ]
--- dashboard/cashflow/views.py
+++ dashboard/cashflow/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Sum
 
-
 from .models import Income
 from .models import Expense
 from .models import IncomeDict
 from .models import ExpenseDict
 
 
+# ----------------------------------------------------------------------------
+
 def index(request):
     return render(request, "index.html")
+
+# ----------------------------------------------------------------------------
 
 def expense(request):
     expense_list = Expense.objects.order_by('year', 'month', 'date')[:]
@@ -20,15 +23,6 @@ def expense(request):
     }
     return render(request, "entry.html", context)
 
-def income(request):
-    income_list = Income.objects.order_by('year', 'month', 'date')[:]
-    context = {
-        'table_name': "Income",
-        'item_list': income_list
-    }
-    return render(request, "entry.html", context)
-
-
 def expensedict(request):
     expense_dict = ExpenseDict.objects.order_by('itemid')[:]
     context = {
@@ -36,15 +30,6 @@ def expensedict(request):
         'item_list': expense_dict
     }
     return render(request, "dict.html", context)
-
-def incomedict(request):
-    income_dict = IncomeDict.objects.order_by('itemid')[:]
-    context = {
-        'table_name': "Income Dictionary",
-        'item_list': income_dict
-    }
-    return render(request, "dict.html", context)
-
 
 def expense_monthly(request):
     expense_monthly = Expense.objects.values('year', 'month').annotate(Sum('amount'))
@@ -55,6 +40,34 @@ def expense_monthly(request):
     return render(request, "report_monthly.html", context)
 
 
+def expense_yearly(request):
+    expense_yearly = Expense.objects.values('year').annotate(Sum('amount'))
+    context = {
+        "table_name": "Expense Yearly",
+        "item_yearly": expense_yearly
+    }
+    return render(request, "report_yearly.html", context)
+
+# ----------------------------------------------------------------------------
+
+def income(request):
+    income_list = Income.objects.order_by('year', 'month', 'date')[:]
+    context = {
+        'table_name': "Income",
+        'item_list': income_list
+    }
+    return render(request, "entry.html", context)
+
+
+def incomedict(request):
+    income_dict = IncomeDict.objects.order_by('itemid')[:]
+    context = {
+        'table_name': "Income Dictionary",
+        'item_list': income_dict
+    }
+    return render(request, "dict.html", context)
+
+
 def income_monthly(request):
     income_monthly = Income.objects.values('year', 'month').annotate(Sum('amount'))
     context = {
@@ -62,3 +75,12 @@ def income_monthly(request):
         "item_monthly": income_monthly
     }
     return render(request, "report_monthly.html", context)
+
+
+def income_yearly(request):
+    income_yearly = Income.objects.values('year').annotate(Sum('amount'))
+    context = {
+        "table_name": "Income Yearly",
+        "item_yearly": income_yearly
+    }
+    return render(request, "report_yearly.html", context)
