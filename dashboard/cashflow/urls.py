c14f1127f5cb47a503e9dffc59e8c5ccb6badd31
K <kwailamchan@hotmail.com>
2018-01-22 21:55:00 +0800

    [master] created dict views and index view. 	new file:   templates/dict.html 	new file:   templates/index.html 	modified:   urls.py 	modified:   views.py

--- dashboard/cashflow/urls.py
+++ dashboard/cashflow/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('expense/', views.expense, name='expense'),
     path('income/', views.income, name='income'),
+    path('expensedict/', views.expensedict, name='expensedict'),
+    path('incomedict/', views.incomedict, name='incomedict'),
 ]
--- dashboard/cashflow/views.py
+++ dashboard/cashflow/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 
 from .models import Income
 from .models import Expense
+from .models import IncomeDict
+from .models import ExpenseDict
+
 
 def index(request):
-    return HttpResponse("Cashflow Platform")
+    return render(request, "index.html")
 
 def expense(request):
     expense_list = Expense.objects.order_by('year', 'month', 'date')[:]
@@ -22,3 +25,20 @@ def income(request):
         'item_list': income_list
     }
     return render(request, "entry.html", context)
+
+
+def expensedict(request):
+    expense_dict = ExpenseDict.objects.order_by('-item')[:]
+    context = {
+        'table_name': "Expense Dictionary",
+        'item_list': expense_dict
+    }
+    return render(request, "dict.html", context)
+
+def incomedict(request):
+    income_dict = IncomeDict.objects.order_by('-item')[:]
+    context = {
+        'table_name': "Income Dictionary",
+        'item_list': income_dict
+    }
+    return render(request, "dict.html", context)
